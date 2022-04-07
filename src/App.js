04716d45b64256d4d5fb1357d73ee1ae4df66895
NG-Joseph <jcool4liv@gmail.com>
2022-04-07 18:50:50 +0100

    despicable commit (sry)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
-  const song = new Audio("./song.mp3");
+  const song = new Audio("./hehe.mp3");
   useEffect(() => {
-    song.play();
-    song.volume = 0.6; // set the volume
-    const msg = new SpeechSynthesisUtterance();
+    song.volume = 0.6;
+    setTimeout((() => {
+      song.play();
+      }), 1000);
+     // set the volume
+   /*  const msg = new SpeechSynthesisUtterance();
     msg.text =
       "Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
     for (let i = 0; i < 20; i++) {
       // play the song   
       window.speechSynthesis.speak(msg);
-    }
+    } */
   });
   return (
     <div className="App">
