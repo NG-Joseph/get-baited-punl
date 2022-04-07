2def6925c718bc7daf45907356e9506147df4814
NG-Joseph <jcool4liv@gmail.com>
2022-04-07 19:20:02 +0100

    omo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
-  const song = new Audio("./hehe.mp3");
-  useEffect(() => {
-    song.volume = 0.6;
-    setTimeout((() => {
-      song.play();
-      }), 1000);
-     // set the volume
-   /*  const msg = new SpeechSynthesisUtterance();
-    msg.text =
-      "Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
+  
+    const msg = new SpeechSynthesisUtterance();
+    let txt = "niggas niggas niggas niggas niggas you are a  fucking neeegaaa "
+    msg.text = txt;
+      //"Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
     for (let i = 0; i < 20; i++) {
-      // play the song   
       window.speechSynthesis.speak(msg);
-    } */
-  });
+    }
+   
   return (
     <div className="App">
       <header className="App-header">
@@ -26,5 +19,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
