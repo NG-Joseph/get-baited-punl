800111f3a5baa5ffb6cb432003ec28c6e361ff32
NG-Joseph <jcool4liv@gmail.com>
2022-04-07 18:12:31 +0100

    small fix... punk

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 
 function App() {
   const song = new Audio("./song.mp3");
   useEffect(() => {
-    song.play(); // play the song
-    song.volume = 0.04; // set the volume
+    song.volume = 0.6; // set the volume
     const msg = new SpeechSynthesisUtterance();
     msg.text =
-      "Never gonna give you up\n never gonna let you down. never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
+      "Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
     for (let i = 0; i < 1000000; i++) {
+    song.play(); // play the song   
       window.speechSynthesis.speak(msg);
     }
   });
