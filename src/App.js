a990ed57429ce0ea567d69af7c2f6945bc441fce
NG-Joseph <jcool4liv@gmail.com>
2022-04-07 18:33:12 +0100

    punkkk

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { useEffect } from "react";
 function App() {
   const song = new Audio("./song.mp3");
   useEffect(() => {
+    song.play();
     song.volume = 0.6; // set the volume
     const msg = new SpeechSynthesisUtterance();
     msg.text =
       "Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
     for (let i = 0; i < 20; i++) {
-     song.play(); // play the song   
+      // play the song   
       window.speechSynthesis.speak(msg);
     }
   });
