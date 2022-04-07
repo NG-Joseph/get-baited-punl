b85e3a53799529e84aff414e66dff6697cd97740
NG-Joseph <jcool4liv@gmail.com>
2022-04-07 18:25:44 +0100

    punkkk

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     const msg = new SpeechSynthesisUtterance();
     msg.text =
       "Never gonna give you up\n never gonna let you down, never gonna run around\n and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you\n";
-    for (let i = 0; i < 1000000; i++) {
-    song.play(); // play the song   
+    for (let i = 0; i < 20; i++) {
+     song.play(); // play the song   
       window.speechSynthesis.speak(msg);
     }
   });
